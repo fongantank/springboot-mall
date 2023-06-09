3e3c7fd6f61cd5d8e5dce46cb523ec0450601530
Tank <fongantank@gmail.com>
2023-06-09 14:15:07 +0800

    商品功能 - 實作查詢商品列表(分頁)

--- src/main/java/com/tank/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/tank/springbootmall/dao/impl/ProductDaoImpl.java
@@ -56,6 +56,7 @@ public class ProductDaoImpl implements ProductDao {
 
     Map<String, Object> map = new HashMap<>();
 
+    // 查詢條件
     if (productQueryParams.getCategory() != null) {
       sql = sql + " and category = :category";
       map.put("category", productQueryParams.getCategory().name());
@@ -66,7 +67,13 @@ public class ProductDaoImpl implements ProductDao {
       map.put("search", "%" + productQueryParams.getSearch() + "%");
     }
 
+    // 排序
     sql = sql + " order by " +productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+    // 分頁
+    sql = sql + " limit :limit offset :offset";
+    map.put("limit", productQueryParams.getLimit());
+    map.put("offset", productQueryParams.getOffset());
 
     List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
     return productList;
